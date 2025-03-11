8f2ad952830da607e8538c2196db86c299046854
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-03-11 15:35:36 +0530

    Registering Mediatr and Carter libraries

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services To App DI Container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 // Configure The Http Request Pipeline
+app.MapCarter();
 
 app.Run();
