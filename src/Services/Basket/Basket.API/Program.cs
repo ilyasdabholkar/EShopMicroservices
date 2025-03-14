ad7079791363dad2363f67e57ec0c389424b55ba
ilyasdabholkar <ilyasdabholkar9@gmail.com>
2025-03-14 16:49:46 +0530

    Register Carter and Mediator libraries

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,8 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add Services To App DI Container
 var assembly = typeof(Program).Assembly;
 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
 var app = builder.Build();
 
 // Configure The Http Request Pipeline
+app.MapCarter();
 
 app.Run();
